4bb27602185bc07986491d9853a3560592da2034
GiSpatial Technology Malaysia <react7tech@gmail.com>
2020-10-10 18:57:12 +0800

    pushy

---
+++ src/screens/DrawerContainer/DrawerContainer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+import MenuButton from '../../components/MenuButton/MenuButton';
+
+export default class DrawerContainer extends React.Component {
+  render() {
+    const { navigation } = this.props;
+    return (
+      <View style={styles.content}>
+        <View style={styles.container}>
+          <MenuButton
+            title="CALENDAR"
+            source={require('../../../assets/icons/home.png')}
+            onPress={() => {
+              navigation.navigate('Search');
+              navigation.closeDrawer();
+            }}
+          />
+          <MenuButton
+            title="ACTIVITIES"
+            source={require('../../../assets/icons/category.png')}
+            onPress={() => {
+              navigation.navigate('Categories');
+              navigation.closeDrawer();
+            }}
+          />
+          <MenuButton
+            title="HEALTH FOOD"
+            source={require('../../../assets/icons/search.png')}
+            onPress={() => {
+              navigation.navigate('BOOTYFIT');
+              navigation.closeDrawer();
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+DrawerContainer.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired
+  })
+};
